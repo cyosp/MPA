b9c56584b3259e93566758f2dfe24b48cd0ae61f
cyosp <cyosp@users.noreply.github.com>
2017-01-08 16:05:06 +0100

    Add MPA server test suite

--- README.md
+++ README.md
@@ -1,13 +1,35 @@
 # MPA
 My Personal Accounts (MPA) is a Web Application to manage personal accounts in a centralized way
 
-# WORK IN PROGRESS
-
 **/!\ WARNING /!\**
 **This project is Work In Progress (WIP) and is not stable for a production use.**
 
-# Server side
+## Server side
 
 Server building environnement is:
  * Eclipse IDE with CDT
  * Debian GNU Linux Jessie x86_64
+
+## TESTS
+
+When MPA binary is built it can be tested to check all is ok.
+
+Tests are driven using [node.js](https://nodejs.org).
+
+### Prerequisites
+
+In order to have a test environment ready to use you must:
+
+ 1. Install [node.js](https://nodejs.org)
+ 2. Move into MPA GitHub directory
+ 3. Run: `npm install --save` to install locally dependencies
+
+### Check MPA server
+
+Before to launch tests you must check MPA:
+
+* Is running on the test machine
+* Can be reached using: http://localhost:8080
+* Login: `cyosp` with password `CYOSP1` is defined
+
+If all is OK, you can execute `npm test` to run MPA server test suite
---
+++ test/MPA.js
@@ -0,0 +1,57 @@
+// 2017-01-08 V 1.0.0
+// - First version
+
+var chai      = require("chai");
+var request   = require("request");
+
+var expect    = chai.expect;
+var assert    = chai.assert;
+
+chai.use( require("chai-http") );
+chai.use( require('chai-json-schema') );
+
+var host = "http://localhost:8080";
+var token = null;
+
+describe( "MPA API" , function()
+{
+	describe( "Login" , function()
+	{
+		it( "returns token" , function( done )
+		{
+			chai.request( host )
+				.post( "/mpa/res/users/login" )
+				.set( 'content-type', 'application/x-www-form-urlencoded' )
+				.send( {login: 'cyosp' , pwd: 'CYOSP1' } )
+			.end( function( error , response , body )
+			{
+				if( error )	done( error );
+				else
+				{
+					var data = JSON.parse( response.text );
+					
+					// Check response is 200
+		   			expect(response.statusCode).to.equal(200);
+		   			
+					// Check JSON contains id property
+					assert.property( data , 'id' );
+		
+					// Get token
+					token = parseInt( data.id );
+		
+					// End test
+		    		done();
+				}
+		  	});
+		});
+	
+		it( "token is a number" , function( done )
+		{
+			// Check token is a number
+			assert.isNumber( token , 'token value' );
+			
+			// End test
+			done();
+		});
+	});
+});
