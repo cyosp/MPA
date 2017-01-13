cf80db22738ddbe912ab82fa9a983797a2797596
cyosp <cyosp@users.noreply.github.com>
2017-01-13 22:25:07 +0100

    Manage register administrator account

--- test/MPA.js
+++ test/MPA.js
@@ -1,3 +1,5 @@
+// 2017-01-13 V 1.1.0
+// - Manage register administrator account
 // 2017-01-08 V 1.0.0
 // - First version
 
@@ -11,10 +13,58 @@ chai.use( require("chai-http") );
 chai.use( require('chai-json-schema') );
 
 var host = "http://localhost:8080";
+var adminLogin = "cyosp";
+var adminPwd = "CYOSP1";
+var userAdminId = null;
 var token = null;
 
 describe( "MPA API" , function()
 {
+	describe( "Register administrator account" , function()
+	{
+		it( "returns user identifier" , function( done )
+		{
+			chai.request( host )
+				.post( "/mpa/res/users/add" )
+				.set( 'content-type', 'application/x-www-form-urlencoded' )
+				.send( {login: adminLogin , password: adminPwd , passwordConfirm: adminPwd } )
+			.end( function( error , response , body )
+			{
+				if( error )	done( error );
+				else
+				{
+					var data = JSON.parse( response.text );
+					
+					// Check response is 200
+		   			expect( response.statusCode ).to.equal(200);
+		   			
+					// Check JSON contains id property
+					assert.property( data , 'id' );
+		
+					// Get token
+					userAdminId = data.id;
+
+					// End test
+		    		done();
+				}
+		  	});
+		});
+		
+		it( "identifier is a positive number" , function( done )
+		{
+			// Check administrator user id is a number
+			expect( userAdminId , 'identifier is not a number' ).to.be.not.NaN;
+			
+			userAdminId = parseInt( userAdminId );
+			
+			// Check administrator user is a positive number
+			expect( Math.abs( userAdminId ) ).to.be.equal( userAdminId );
+			
+			// End test
+			done();
+		});
+	});
+
 	describe( "Login" , function()
 	{
 		it( "returns token" , function( done )
@@ -22,7 +72,7 @@ describe( "MPA API" , function()
 			chai.request( host )
 				.post( "/mpa/res/users/login" )
 				.set( 'content-type', 'application/x-www-form-urlencoded' )
-				.send( {login: 'cyosp' , pwd: 'CYOSP1' } )
+				.send( {login: adminLogin , pwd: adminPwd } )
 			.end( function( error , response , body )
 			{
 				if( error )	done( error );
