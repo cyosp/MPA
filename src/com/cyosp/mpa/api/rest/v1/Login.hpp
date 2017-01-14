1fd894d37db19036c384207bfce2826ae7d6919f
cyosp <cyosp@users.noreply.github.com>
2017-01-14 15:54:24 +0100

    Add Login class

---
+++ src/com/cyosp/mpa/api/rest/v1/Login.hpp
@@ -0,0 +1,41 @@
+/*
+ * Login.hpp
+ *
+ *  Created on: 2017-01-04
+ *      Author: CYOSP
+ */
+
+#ifndef INCLUDES_MPA_LOGIN_HPP_
+#define INCLUDES_MPA_LOGIN_HPP_
+
+#include "com/cyosp/mpa/MPA.hpp"
+#include "com/cyosp/mpa/MPAO.hpp"
+#include "com/cyosp/rest/HTTPHandler.hpp"
+
+namespace mpa_api_rest_v1
+{
+
+class Login: public mpa::MPAO
+{
+
+	protected:
+
+		bool areGetParametersOk();
+		bool arePostLoginParametersOk();
+		bool arePostAddParametersOk();
+
+		string executeGetRequest(ptree & root);
+		string executePostLoginRequest(ptree & root);
+		string executePostAddRequest(ptree & root);
+		string executePostDeleteRequest(ptree & root);
+		string executePostUpdateRequest(ptree & root);
+
+	public:
+		Login( HttpRequestType httpRequestType, ActionType actionType, const map<string, string>& argvals, bool isAdmin, vector<std::pair<string, int> > urlPairs );
+
+	virtual ~Login();
+};
+
+} /* namespace mpa */
+
+#endif /* INCLUDES_MPA_LOGIN_HPP_ */
